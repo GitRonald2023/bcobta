3343f983459fb81011876ffd6baf91609419142f
ronald_rodriguez <ronaldgonzalorodriguezgrosso@gmail.com>
2024-08-22 22:14:23 -0500

    Inicio Proyecto

---
+++ src/main/java/com/bcobta/bcobta/BcobtaApplication.java
@@ -0,0 +1,13 @@
+package com.bcobta.bcobta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcobtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcobtaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bcobta/bcobta/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package com.bcobta.bcobta;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.bcobta.bcobta.controllers.HomeController;
+
+@SpringBootTest
+public class HomeControllerTest {
+
+    @Autowired
+    HomeController homeController;
+
+    @Test
+    public void homeController_healthCheck_returnsOk() {
+        assertEquals(homeController.healthCheck(), "OK");
+    }
+    
+    @Test
+    public void homeController_getPeople_returnsListOf20People() {
+        assertEquals(homeController.getPeople().size(), 20);
+    }
+}
